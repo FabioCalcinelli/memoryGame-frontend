7606d926527d101ec74f2457f69d18d71d862907
fabio <fabio.calcinelli@tngtech.com>
2025-01-21 07:39:12 +0100

    Step 3 in progress, needs further refactoring and debugging

--- memoryGame/src/Field/Field.tsx
+++ memoryGame/src/Field/Field.tsx
@@ -1,5 +1,5 @@
 import Card from "../Card/Card.tsx"
-import {HTMLAttributes, useMemo, useState} from "react";
+import {Dispatch, HTMLAttributes, SetStateAction, useMemo, useState} from "react";
 import {generateRandomPairs} from "../helper/generateRandomPairs.tsx";
 
 interface FieldProps extends HTMLAttributes<HTMLDivElement> {
@@ -13,29 +13,8 @@ export function Field({numCards, className, ...divProps}: FieldProps) {
     const arrayOfCardNumbers = useMemo(() => generateRandomPairs(numCards / 2), []);
     const handleCardClick = (cardIndex: number) => {
         return () => setFlipped(
-            (oldState: boolean[]): boolean[] => {
-                const newState: boolean[] = [...oldState]
-                const numberOfFlipped: number = flipped.length
-                if (!oldState[cardIndex]) {
-                    if (numberOfFlipped % 2 == 0 && numberOfFlipped !== 0) {
-                        const flippedNotFoundArray: boolean[] = oldState.map((a, i) => a && !found[i]);
-                        const indexesToUnflip: (number | null)[] = flippedNotFoundArray.map((x, i) => x ? i : null).filter(x => x !== null);
-                        indexesToUnflip.forEach((x) => {
-                            newState[x] = false
-                        })
-                    } else {
-                        if (arrayOfCardNumbers[cardIndex] == arrayOfCardNumbers[oldState.indexOf(true)]) {
-                            newState[cardIndex] = !oldState[cardIndex]
-                            setFound((oldFoundState: boolean[]): boolean[] => {
-                                const newFoundState: boolean[] = [...oldFoundState]
-                                newFoundState[cardIndex] = true;
-                                newFoundState[oldState.indexOf(true)] = true;
-                                return newFoundState
-                            })
-                        }
-                    }
-                }
-                return newState
+            (currentState: boolean[]): boolean[] => {
+                return updateField(cardIndex, currentState, found, setFound, arrayOfCardNumbers)
             }
         )
     }
@@ -45,4 +24,42 @@ export function Field({numCards, className, ...divProps}: FieldProps) {
                                                     onCardClicked={handleCardClick(j)}/>)}
         </div>
     );
+}
+
+function updateField(cardIndex: number, flipped: boolean[], found: boolean[],
+                     setFound: Dispatch<SetStateAction<boolean[]>>, arrayOfCardNumbers: number[]) : boolean[] {
+    let newFlipped: boolean[] = [...flipped]
+    const numberOfFlipped: number = flipped.length
+    const cardIsCovered: boolean = !flipped[cardIndex]
+    if (cardIsCovered) {
+        const thirdCardClicked: boolean = numberOfFlipped % 2 == 0 && numberOfFlipped !== 0
+        if (thirdCardClicked) {
+            newFlipped = unFlipBothCards(flipped, flipped, found)
+        } else {
+            const pairFound: boolean = arrayOfCardNumbers[cardIndex] == arrayOfCardNumbers[flipped.indexOf(true)]
+            if (pairFound) {
+                newFlipped[cardIndex] = true
+                addPairToFound(newFlipped, cardIndex, flipped, setFound)
+            }
+        }
+    }
+    return newFlipped
+}
+
+function unFlipBothCards(flipped: boolean[], newFlipped: boolean[], found: boolean[]) : boolean[] {
+    const flippedNotFoundArray: boolean[] = flipped.map((a, i) => a && !found[i]);
+    const indexesToUnflip: (number | null)[] = flippedNotFoundArray.map((x, i) => x ? i : null).filter(x => x !== null);
+    indexesToUnflip.forEach((x) => {
+        newFlipped[x] = false
+    })
+    return flipped
+}
+
+function addPairToFound(newFlipped: boolean[], cardIndex: number, flipped: boolean[], setFound: Dispatch<SetStateAction<boolean[]>>) {
+    setFound((oldFoundState: boolean[]): boolean[] => {
+        const newFoundState: boolean[] = [...oldFoundState]
+        newFoundState[cardIndex] = true;
+        newFoundState[flipped.indexOf(true)] = true;
+        return newFoundState
+    })
 }
